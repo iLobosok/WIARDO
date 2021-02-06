f7f112c65cd75f28997bb529ee14909c649b3e6b
ilobosok <48067632+iLobosok@users.noreply.github.com>
2021-02-06 13:16:44 +0300

    Update Shop.dart

--- lib/ui/Shop.dart
+++ lib/ui/Shop.dart
@@ -51,6 +51,7 @@ class Shopping extends State<Shop> {
       'https://firebasestorage.googleapis.com/v0/b/chat-b43aa.appspot.com/o/images%2Fid-0xiGjOHeZwfHJNxCHJJMtryEZjp2%2Fhash-74849519?alt=media&token=466d8ff0-c712-46e4-a5d4-1762a30f2a0b',
       'https://firebasestorage.googleapis.com/v0/b/chat-b43aa.appspot.com/o/images%2Fid-0xiGjOHeZwfHJNxCHJJMtryEZjp2%2Fhash-45474279?alt=media&token=c8400e7a-33c4-4ccf-8184-3759feb8c936',
     ];
+    //test
     uploadImage() async {
       final _firebaseStorage = FirebaseStorage.instance;
       final _imagePicker = ImagePicker();
