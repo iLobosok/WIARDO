0699decbfad5e46347878b6b3111d4b143f47f93
Ivan <ilobosoki@gmail.com>
2021-02-16 21:46:11 +0300

    Добавил проверку на seller ? true or false; Добавил присваивание рандомных картинок, если нету авы

--- lib/database/Data.dart
+++
@@ -1,4 +0,0 @@
-class Data{
-  String img, name, cost, type,description;
-  Data({this.img, this.name, this.cost, this.type, this.description});
-}
--- lib/model/config.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-class Paddings{
-  static double getPadding(context, double pad){
-    var Padding_1 = MediaQuery.of(context).size.width * pad;
-    return Padding_1;
-  }
-  
-}
