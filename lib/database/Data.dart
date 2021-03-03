c201402ace3ef7aab8aba1567b5a34652b02151e
EcsFlash <55760802+EcsFlash@users.noreply.github.com>
2021-03-03 21:52:33 +0300

    Add files via upload

--- lib/database/Data.dart
+++ lib/database/Data.dart
@@ -1,4 +1,4 @@
 class Data{
-  String img, name, cost, type,description, inst;
-  Data({this.img, this.name, this.cost, this.type, this.description, this.inst});
+  String img, name, cost, type,description, inst, productID;
+  Data({this.img, this.name, this.cost, this.type, this.description, this.inst, this.productID});
 }
